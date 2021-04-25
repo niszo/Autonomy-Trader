cf7e8487e32d6fb0a595d9f035d172002f0d47bc
Nick Wodzisz <82302381+WodziszNick@users.noreply.github.com>
2021-04-25 02:09:01 -0400

    Add files via upload

---
+++ populate_stocks.py
@@ -0,0 +1,30 @@
+import sqlite3, config
+import alpaca_trade_api as tradeapi
+
+connection = sqlite3.connect(config.DB_FILE)
+connection.row_factory = sqlite3.Row
+
+
+cursor = connection.cursor()
+
+cursor.execute("""
+    SELECT symbol, name FROM stock
+""")
+rows = cursor.fetchall()
+symbols = [row['symbol'] for row in rows]
+
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL) # or use ENV Vars shown below
+assets = api.list_assets()
+
+for asset in assets:
+    try:
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            cursor.execute("INSERT INTO stock (symbol, name) VALUES (?, ?)", (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+
+connection.commit()
+
+
+
