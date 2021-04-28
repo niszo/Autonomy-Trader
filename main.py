f8b392759eff4b4c34e5503512234642c9d8957c
Nick Wodzisz <82302381+WodziszNick@users.noreply.github.com>
2021-04-27 23:57:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3, config
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from datetime import date
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -8,15 +10,27 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def index(request: Request):
+    stock_filter = request.query_params.get('filter', False)
     connection = sqlite3.connect(config.DB_FILE)
     connection.row_factory = sqlite3.Row
 
 
     cursor = connection.cursor()
 
-    cursor.execute("""
-        SELECT id, symbol, name FROM stock ORDER by symbol
-    """)
+    if stock_filter == 'new_closing_highs':
+        cursor.execute("""
+        select * from (
+            select symbol, name, stock_id, max(close), date
+            from stock_price join stock on stock.id = stock_price.stock_id
+            group by stock_id
+            order by symbol
+        ) where date = ?
+        """, (date.today().isoformat(),))
+    else:
+
+        cursor.execute("""
+            SELECT id, symbol, name FROM stock ORDER by symbol
+        """)
     rows = cursor.fetchall()
 
     return templates.TemplateResponse("index.html", {"request": request, "stocks": rows})
@@ -26,6 +40,13 @@ def stock_detail(request: Request, symbol):
     connection = sqlite3.connect(config.DB_FILE)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT * FROM strategy
+    """)
+
+    strategies = cursor.fetchall()
+
     cursor.execute("""
         SELECT id, symbol, name FROM stock WHERE symbol = ?
     """, (symbol,))
@@ -37,7 +58,39 @@ def stock_detail(request: Request, symbol):
     """, (row['id'],))
 
     prices = cursor.fetchall()
-    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": prices})
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": prices, "strategies": strategies})
+
+
+    
+@app.get("/apply_strategy")
+def apply_strategy(strategy_id: int = Form(...), stock_id: int = Form(...)):
+    connection = sqlite3.connect(config.DB_FILE)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO stock_strategy (stock_id, strategy_id) VALUES (?, ?)
+    """, (stock_id, strategy_id))
+
+    connection.commit()
+
+    return RedirectResponse(url=f"/strategy/{strategy_id}", status_code =303)
+
+@app.get("/strategy/{strategy_id}")
+def strategy(request: Request, strategy_id):
+    connection = sqlite3.connect(config.DB_FILE)
+    connection.row_factory = sqlite3.Row
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, name
+        FROM strategy
+        WHERE id = ?
+    """, (strategy_id,))
+    
+    stocks = cursor.fetchall()
+
+    return templates.TemplateResponse("strategy.html", {"request": request, "stocks": stocks, "strategy": strategy})
 
 
     
