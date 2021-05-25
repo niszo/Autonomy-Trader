3a9cda1f48ede7837ca3d99bbbbf9cf909f78b88
Nick Wodzisz <82302381+nick-wodzisz@users.noreply.github.com>
2021-05-24 20:47:08 -0400

    Update populate_prices.py

--- populate_prices.py
+++ populate_prices.py
@@ -1,5 +1,6 @@
 import sqlite3, config
 import alpaca_trade_api as tradeapi
+
 connection = sqlite3.connect('app.db')
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
@@ -13,13 +14,13 @@ for row in rows:
     symbol = row['symbol']
     symbols.append(symbol)
     stock_dict[symbol] = row['id']
-api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL)
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL) 
 chunk_size = 200
 for i in range(0, len(symbols), chunk_size):
     symbol_chunk = symbols[i:i+chunk_size]
     barsets = api.get_barset(symbol_chunk, 'day')
     for symbol in barsets:
-        print(f"processing symbol {symbol}")
+        print(f"processing symbol {symbol}") 
         for bar in barsets[symbol]:
             stock_id = stock_dict[symbol]
             cursor.execute("""
@@ -27,4 +28,3 @@ for i in range(0, len(symbols), chunk_size):
                 VALUES (?, ?, ?, ?, ?, ?, ?)
             """, (stock_id, bar.t.date(), bar.o, bar.h, bar.l, bar.c, bar.v))
 connection.commit()  
-
