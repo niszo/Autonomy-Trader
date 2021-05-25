ebcdc310872d758b007b848c89d18ccd962e44be
Nick Wodzisz <82302381+nick-wodzisz@users.noreply.github.com>
2021-05-24 20:46:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-API_KEY ='PKDDPIU0E6SOTH8ITD8Z'
-SECRET_KEY = 'QMcEkwdfLsMFtq6cMzMzRTj4Ia2wJVHf76B5fGyf'
+API_KEY ='confidential'
+SECRET_KEY = 'confidential'
 API_URL = 'https://paper-api.alpaca.markets'
 DB_FILE ='app.db' 
+
+EMAIL_ADDRESS = 'confidential@kent.edu'
+EMAIL_SMS = 'confidential@messaging.sprintpcs.com'
+EMAIL_PASSWORD = 'confidential'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
